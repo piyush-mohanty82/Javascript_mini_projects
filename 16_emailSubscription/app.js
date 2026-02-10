7eda92e2df6a3a0e9919d2f5406c107ba3ab2150
Piyush Mohanty <piyushmohanty509@gmail.com>
2026-02-10 18:13:20 +0530

    email subscription

---
+++ 16_emailSubscription/app.js
@@ -0,0 +1,16 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwfA-nBVmcxWOVoamIMhy5Bs8DmWdHPyxKsSZwcaNcdmba-TQLIJgkNkyD1lfr15kl2/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById('msg');
+	form.addEventListener('submit', e => {
+		e.preventDefault()
+		fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+			.then(response => response.json())
+			.then(response => {
+                msg.innerHTML = "Thank You For Subscribing!"
+                setTimeout(function(){
+                    msg.innerHTML = ""
+                }, 5000)
+                form.reset()
+            })
+			.catch(error => console.error('Error!', error.message))
+	})
